36aab7f8a06eb61b0b788db3332bf7efc19ae568
Alex Stancu <alex.stancu96@outlook.com>
2020-04-11 14:41:20 +0300

    added spring-initializer skeleton

---
+++ src/main/java/com/springboot/simpleatm/SimpleAtmApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.simpleatm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAtmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/simpleatm/SimpleAtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.simpleatm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
