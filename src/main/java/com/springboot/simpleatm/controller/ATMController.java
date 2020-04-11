3eb9a877559f7a42cf5bc5a81022784c6e08d365
Alex Stancu <alex.stancu96@outlook.com>
2020-04-11 16:21:32 +0300

    set up application.properties for the db; added sql script to initialize the db; added empty atm controller

---
+++ src/main/java/com/springboot/simpleatm/controller/ATMController.java
@@ -0,0 +1,10 @@
+package com.springboot.simpleatm.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ATMController {
+    Logger logger = LoggerFactory.getLogger(ATMController.class);
+}
