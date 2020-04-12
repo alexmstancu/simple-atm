801319119d4107caf4fe9c2e740562e16d781b8c
Alex Stancu <alex.stancu96@outlook.com>
2020-04-12 20:48:56 +0300

    write full unit tests for account service

--- src/test/java/com/springboot/simpleatm/account/BasicUserAccountServiceTest.java
+++ src/test/java/com/springboot/simpleatm/account/BasicUserAccountServiceTest.java
@@ -1,5 +1,7 @@
 package com.springboot.simpleatm.account;
 
+import com.springboot.simpleatm.exception.InsufficientBalanceException;
+import com.springboot.simpleatm.exception.InvalidAmountException;
 import com.springboot.simpleatm.exception.UserAccountNotFoundException;
 import com.springboot.simpleatm.model.UserAccount;
 import com.springboot.simpleatm.repository.UserAccountRepository;
@@ -16,8 +18,9 @@ import static org.junit.Assert.assertEquals;
 @RunWith(JMockit.class)
 public class BasicUserAccountServiceTest {
     private static final String ACCOUNT_NUMBER = "12345";
-    private static final Double INITIAL_BALANCE = 2000D;
-
+    private static final Double INITIAL_BALANCE = 5000D;
+    private static final Double VALID_AMOUNT = 1000D;
+    private static final Double NEGATIVE_AMOUNT = -1000D;
 
     @Injectable
     private UserAccountRepository userAccountRepository;
@@ -27,19 +30,19 @@ public class BasicUserAccountServiceTest {
 
     @Test
     public void testFetchAccountDetails() {
-        UserAccount dbUserAccount = createUserAccount(ACCOUNT_NUMBER, INITIAL_BALANCE);
+        UserAccount dbUserAccount = createUserAccount();
 
         new Expectations() {{
             userAccountRepository.findByAccountNumber(ACCOUNT_NUMBER);
             result = dbUserAccount;
         }};
 
-        UserAccount serviceUserAccount = userAccountService.fetchAccountDetails(ACCOUNT_NUMBER);
-        assertEquals(dbUserAccount, serviceUserAccount);
+        UserAccount servicedUserAccount = userAccountService.fetchAccountDetails(ACCOUNT_NUMBER);
+        assertEquals(dbUserAccount, servicedUserAccount);
     }
 
     @Test(expected = UserAccountNotFoundException.class)
-    public void givenAccountDoesNotExist_whenFetchAccountDetails_thenThrow() {
+    public void testFetchAccountDetailsWhenAccountNotFound() {
         new Expectations() {{
             userAccountRepository.findByAccountNumber(ACCOUNT_NUMBER);
             result = null;
@@ -50,36 +53,108 @@ public class BasicUserAccountServiceTest {
 
     @Test
     public void testDeposit() {
-        UserAccount dbUserAccount = createUserAccount(ACCOUNT_NUMBER, INITIAL_BALANCE);
-        Double depositedAmount = 1000D;
+        UserAccount dbUserAccount = createUserAccount();
 
         new Expectations() {{
             userAccountRepository.findByAccountNumber(ACCOUNT_NUMBER);
             result = dbUserAccount;
         }};
 
-        UserAccount serviceUserAccount = userAccountService.deposit(ACCOUNT_NUMBER, depositedAmount);
+        UserAccount servicedUserAccount = userAccountService.deposit(ACCOUNT_NUMBER, VALID_AMOUNT);
 
         new Verifications() {{
             UserAccount savedAccount;
             userAccountRepository.save(savedAccount = withCapture());
             times = 1;
 
-            assertEquals(serviceUserAccount, savedAccount);
+            assertEquals(servicedUserAccount, savedAccount);
             assertEquals(dbUserAccount.getId(), savedAccount.getId());
             assertEquals(dbUserAccount.getName(), savedAccount.getName());
             assertEquals(dbUserAccount.getAccountNumber(), savedAccount.getAccountNumber());
             assertEquals(dbUserAccount.getPin(), savedAccount.getPin());
-            Double expectedBalance = INITIAL_BALANCE + depositedAmount;
+            Double expectedBalance = INITIAL_BALANCE + VALID_AMOUNT;
             assertEquals(expectedBalance, savedAccount.getBalance());
 
         }};
     }
 
-    private UserAccount createUserAccount(String accountNumber, Double balance) {
+    @Test(expected = UserAccountNotFoundException.class)
+    public void testDepositWhenAccountNotFound() {
+        new Expectations() {{
+            userAccountRepository.findByAccountNumber(ACCOUNT_NUMBER);
+            result = null;
+        }};
+
+        userAccountService.deposit(ACCOUNT_NUMBER, VALID_AMOUNT);
+    }
+
+    @Test(expected = InvalidAmountException.class)
+    public void testDepositWhenAmountIsZero() {
+        userAccountService.deposit(ACCOUNT_NUMBER, 0D);
+    }
+
+    @Test(expected = InvalidAmountException.class)
+    public void testDepositWhenAmountIsNegative() {
+        userAccountService.deposit(ACCOUNT_NUMBER, NEGATIVE_AMOUNT);
+    }
+
+    @Test(expected = InvalidAmountException.class)
+    public void testDepositWhenAmountIsNull() {
+        userAccountService.deposit(ACCOUNT_NUMBER, null);
+    }
+
+    @Test
+    public void testWithdraw() {
+        UserAccount dbUserAccount = createUserAccount();
+
+        new Expectations() {{
+            userAccountRepository.findByAccountNumber(ACCOUNT_NUMBER);
+            result = dbUserAccount;
+        }};
+
+        UserAccount servicedUserAccount = userAccountService.withdraw(ACCOUNT_NUMBER, VALID_AMOUNT);
+
+        new Verifications() {{
+            UserAccount savedAccount;
+            userAccountRepository.save(savedAccount = withCapture());
+            times = 1;
+
+            assertEquals(servicedUserAccount, savedAccount);
+            assertEquals(dbUserAccount.getId(), savedAccount.getId());
+            assertEquals(dbUserAccount.getName(), savedAccount.getName());
+            assertEquals(dbUserAccount.getAccountNumber(), savedAccount.getAccountNumber());
+            assertEquals(dbUserAccount.getPin(), savedAccount.getPin());
+            Double expectedBalance = INITIAL_BALANCE - VALID_AMOUNT;
+            assertEquals(expectedBalance, savedAccount.getBalance());
+
+        }};
+    }
+
+    @Test(expected = InvalidAmountException.class)
+    public void testWithdrawWhenAmountIsZero() {
+        userAccountService.withdraw(ACCOUNT_NUMBER, 0D);
+    }
+
+    @Test(expected = InvalidAmountException.class)
+    public void testWithdrawWhenAmountIsNegative() {
+        userAccountService.withdraw(ACCOUNT_NUMBER, NEGATIVE_AMOUNT);
+    }
+
+    @Test(expected = InvalidAmountException.class)
+    public void testWithdrawWhenAmountIsNull() {
+        userAccountService.withdraw(ACCOUNT_NUMBER, null);
+    }
+
+    @Test(expected = InsufficientBalanceException.class)
+    public void testWithdrawWhenInsufficientBalance() {
+        Double withdrawAmount = INITIAL_BALANCE + 1D;
+        userAccountService.withdraw(ACCOUNT_NUMBER, withdrawAmount);
+    }
+
+    private UserAccount createUserAccount() {
         return UserAccount.builder()
-                .accountNumber(accountNumber)
-                .balance(balance)
+                .accountNumber(ACCOUNT_NUMBER)
+                .balance(INITIAL_BALANCE)
                 .name("dummy")
                 .pin("1234")
                 .id("someId")
