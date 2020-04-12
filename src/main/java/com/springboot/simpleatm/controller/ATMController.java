5af4b65a4bd11752f8dc4db53af19492fd542f7b
Alex Stancu <alex.stancu96@outlook.com>
2020-04-12 17:19:48 +0300

    delete builder methods and refactor around that & remove account number from the withdraw and deposit responses

--- src/main/java/com/springboot/simpleatm/controller/ATMController.java
+++ src/main/java/com/springboot/simpleatm/controller/ATMController.java
@@ -2,11 +2,12 @@ package com.springboot.simpleatm.controller;
 
 import com.springboot.simpleatm.account.UserAccountService;
 import com.springboot.simpleatm.model.UserAccount;
-import com.springboot.simpleatm.model.operation.*;
+import com.springboot.simpleatm.model.operation.OperationPayload;
+import com.springboot.simpleatm.model.operation.OperationResult;
+import com.springboot.simpleatm.model.operation.OperationType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -27,32 +28,32 @@ public class ATMController {
     }
 
     @GetMapping("/{accountNumber}/details")
-    public ResponseEntity<OperationResult> getAccountDetails(@PathVariable String accountNumber) {
+    public OperationResult getAccountDetails(@PathVariable String accountNumber) {
         logger.debug("Requesting account details for account number {}", accountNumber);
         UserAccount account = userAccountService.fetchAccountDetails(accountNumber);
         logger.debug("Account details successfully fetched for account number {}, current balance {}, holder name '{}'", accountNumber, account.getBalance(), account.getName());
 
-        OperationResult operationResult = OperationResult.builder()
+        return OperationResult.builder()
                 .operationType(OperationType.accountDetails.name())
                 .accountNumber(accountNumber)
                 .currentBalance(account.getBalance())
                 .holderName(account.getName())
                 .build();
-
-        return ResponseEntity.ok(operationResult);
     }
 
     @PostMapping("/{accountNumber}/withdraw")
-    public ResponseEntity<OperationResult> withdraw(@PathVariable String accountNumber, @RequestBody OperationPayload payload) {
+    public OperationResult withdraw(@PathVariable String accountNumber, @RequestBody OperationPayload payload) {
         Double amount = payload.getAmount();
 
         logger.debug("Requesting withdrawal with amount {} in currency {} for account number {}", amount, payload.getCurrency(), accountNumber);
         UserAccount account = userAccountService.withdraw(accountNumber, amount);
         logger.debug("Withdrawal successful with amount {} for account number {}, current balance {}", amount, accountNumber, account.getBalance());
 
-        OperationResult operationResult = buildResult(accountNumber, account.getBalance(), OperationType.withdraw, amount);
-        return ResponseEntity.ok(operationResult);
-
+        return OperationResult.builder()
+                .operationType(OperationType.withdraw.name())
+                .operatedAmount(amount)
+                .currentBalance(account.getBalance())
+                .build();
     }
 
     @PostMapping("/{accountNumber}/deposit")
@@ -63,19 +64,10 @@ public class ATMController {
         UserAccount account = userAccountService.deposit(accountNumber, amount);
         logger.debug("Deposit successful with amount {} for account number {}, current balance {}", amount, accountNumber, account.getBalance());
 
-        return buildResult(accountNumber, account.getBalance(), OperationType.deposit, amount);
-    }
-
-    /**
-     * Creates an {@link OperationResult} object having all the fields set with the given parameters.
-     * See {@link OperationType}, {@link OperationErrorMessage}
-     */
-    private OperationResult buildResult(String accountNumber, Double balance, OperationType type, Double operatedAmount) {
         return OperationResult.builder()
-                .operationType(type.name())
-                .operatedAmount(operatedAmount)
-                .accountNumber(accountNumber)
-                .currentBalance(balance)
+                .operationType(OperationType.deposit.name())
+                .operatedAmount(amount)
+                .currentBalance(account.getBalance())
                 .build();
     }
 }
