b55377958efc5ac7fdb46f02c02cb116c8d43a8d
Alex Stancu <alex.stancu96@outlook.com>
2020-04-12 12:24:01 +0300

    refactor the result builder methods and improve logging, also write some javadoc

--- src/main/java/com/springboot/simpleatm/controller/ATMController.java
+++ src/main/java/com/springboot/simpleatm/controller/ATMController.java
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/userAccount")
+@RequestMapping("/atm/userAccount")
 public class ATMController {
-    Logger logger = LoggerFactory.getLogger(ATMController.class);
+    private static final Logger logger = LoggerFactory.getLogger(ATMController.class);
 
-    private UserAccountService userAccountService;
+    private final UserAccountService userAccountService;
 
     @Autowired
     public ATMController(UserAccountService userAccountService) {
@@ -35,9 +35,9 @@ public class ATMController {
 
         try {
             logger.debug("Requesting account details for account number {}", accountNumber);
-            UserAccount userAccount = userAccountService.fetchAccountDetails(accountNumber);
-            logger.debug("Account details successfully fetched for account number {}", accountNumber);
-            return buildSuccessfulResult(userAccount, operationType);
+            UserAccount account = userAccountService.fetchAccountDetails(accountNumber);
+            logger.debug("Account details successfully fetched for account number {}, current balance {}, holder name '{}'", accountNumber, account.getBalance(), account.getName());
+            return buildSuccessfulResult(account, operationType);
         } catch (UserAccountNotFoundException e) {
             logger.error("Account number {} not found", accountNumber, e);
             errorMessage = OperationErrorMessage.ACCOUNT_NOT_FOUND;
@@ -56,10 +56,9 @@ public class ATMController {
         String errorMessage;
 
         try {
-            String currency = payload.getCurrency();
-            logger.debug("Requesting withdrawal with amount {} in currency {} for account number {}", amount, currency, accountNumber);
+            logger.debug("Requesting withdrawal with amount {} in currency {} for account number {}", amount, payload.getCurrency(), accountNumber);
             UserAccount userAccount = userAccountService.withdraw(accountNumber, amount);
-            logger.debug("Withdrawal successful with amount {} for account number {}", amount, accountNumber);
+            logger.debug("Withdrawal successful with amount {} for account number {}, current balance {}", amount, accountNumber, userAccount.getBalance());
             return buildSuccessfulResult(userAccount, operationType, amount);
         } catch (UserAccountNotFoundException e) {
             logger.error("Account number {} not found", accountNumber, e);
@@ -75,7 +74,7 @@ public class ATMController {
             errorMessage = OperationErrorMessage.COULD_NOT_PROCESS;
         }
 
-        return buildUnsuccessfulResult(accountNumber, operationType, errorMessage);
+        return buildUnsuccessfulResult(accountNumber, operationType, amount, errorMessage);
     }
 
     @PostMapping("/{accountNumber}/deposit")
@@ -85,10 +84,9 @@ public class ATMController {
         String errorMessage;
 
         try {
-            String currency = payload.getCurrency();
-            logger.debug("Requesting deposit with amount {} in currency {} for account number {}", amount, currency, accountNumber);
+            logger.debug("Requesting deposit with amount {} in currency {} for account number {}", amount, payload.getCurrency(), accountNumber);
             UserAccount userAccount = userAccountService.deposit(accountNumber, amount);
-            logger.debug("Deposit successful with amount {} for account number {}", amount, accountNumber);
+            logger.debug("Deposit successful with amount {} for account number {}, current balance {}", amount, accountNumber, userAccount.getBalance());
             return buildSuccessfulResult(userAccount, operationType, amount);
         } catch (UserAccountNotFoundException e) {
             logger.error("Account number {} not found", accountNumber, e);
@@ -101,30 +99,69 @@ public class ATMController {
             errorMessage = OperationErrorMessage.COULD_NOT_PROCESS;
         }
 
-        return buildUnsuccessfulResult(accountNumber, operationType, errorMessage);
+        return buildUnsuccessfulResult(accountNumber, operationType, amount, errorMessage);
     }
 
-
-    private OperationResult buildSuccessfulResult(UserAccount userAccount, OperationType type) {
-        return buildSuccessfulResult(userAccount, type, null);
+    /**
+     * Creates a successful {@link OperationResult} object having the following fields set:
+     * - operationType (see {@link OperationType}
+     * - operationStatus (equal to {@link OperationStatus#succeeded})
+     * - currentBalance
+     * - accountNumber
+     * - holderName
+     */
+    private OperationResult buildSuccessfulResult(UserAccount account, OperationType type) {
+        return buildResult(account.getAccountNumber(), account.getBalance(), type, null, OperationStatus.succeeded, account.getName(), null);
     }
 
-    private OperationResult buildSuccessfulResult(UserAccount userAccount, OperationType type, Double operatedAmount) {
-        return OperationResult.builder()
-                .operationType(type.name())
-                .operationStatus(OperationStatus.succeeded.name())
-                .operatedAmount(operatedAmount)
-                .accountNumber(userAccount.getAccountNumber())
-                .currentBalance(userAccount.getBalance())
-                .holderName(userAccount.getName())
-                .build();
+    /**
+     * Creates a successful {@link OperationResult} object having the following fields populated from the given parameters:
+     * - operationType (see {@link OperationType}
+     * - operationStatus (equal to {@link OperationStatus#succeeded})
+     * - operatedAmount
+     * - currentBalance
+     * - accountNumber
+     */
+    private OperationResult buildSuccessfulResult(UserAccount account, OperationType type, Double operatedAmount) {
+        return buildResult(account.getAccountNumber(), account.getBalance(), type, operatedAmount, OperationStatus.succeeded, null, null);
+
     }
 
+    /**
+     * Creates an unsuccessful {@link OperationResult} object having the following fields set:
+     * - operationType (see {@link OperationType}
+     * - operationStatus (equal to {@link OperationStatus#failed})
+     * - error (see {@link OperationErrorMessage}
+     * - accountNumber
+     */
     private OperationResult buildUnsuccessfulResult(String accountNumber, OperationType type, String errorMessage) {
+        return buildResult(accountNumber, null, type, null, OperationStatus.failed, null, errorMessage);
+    }
+
+    /**
+     * Creates an unsuccessful {@link OperationResult} object having the following fields set:
+     * - operationType (see {@link OperationType}
+     * - operationStatus (equal to {@link OperationStatus#failed})
+     * - error (see {@link OperationErrorMessage}
+     * - operatedAmount
+     * - accountNumber
+     */
+    private OperationResult buildUnsuccessfulResult(String accountNumber, OperationType type, Double operatedAmount, String errorMessage) {
+        return buildResult(accountNumber, null, type, operatedAmount, OperationStatus.failed, null, errorMessage);
+    }
+
+    /**
+     * Creates an {@link OperationResult} object having all the fields set with the given parameters.
+     * See {@link OperationType}, {@link OperationStatus}, {@link OperationErrorMessage}
+     */
+    private OperationResult buildResult(String accountNumber, Double balance, OperationType type, Double operatedAmount, OperationStatus status, String name, String errorMessage) {
         return OperationResult.builder()
-                .accountNumber(accountNumber)
                 .operationType(type.name())
-                .operationStatus(OperationStatus.failed.name())
+                .operationStatus(status.name())
+                .operatedAmount(operatedAmount)
+                .accountNumber(accountNumber)
+                .currentBalance(balance)
+                .holderName(name)
                 .error(errorMessage)
                 .build();
     }
--- src/main/java/com/springboot/simpleatm/exception/InsufficientBalanceException.java
+++ src/main/java/com/springboot/simpleatm/exception/InsufficientBalanceException.java
@@ -2,6 +2,6 @@ package com.springboot.simpleatm.exception;
 
 public class InsufficientBalanceException extends Exception {
     public InsufficientBalanceException(Double currentBalance, Double amount) {
-        super("current balance: " + currentBalance + ", amount to be withdrawn: " + amount);
+        super("current balance: " + currentBalance + ", requested withdraw amount: " + amount);
     }
 }
