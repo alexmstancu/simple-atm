8d38e767001206146659c1a0a2bbeebab32071b0
Alex Stancu <alex.stancu96@outlook.com>
2020-04-13 16:36:29 +0300

    remove unused imports

--- src/main/java/com/springboot/simpleatm/SimpleAtmApplication.java
+++ src/main/java/com/springboot/simpleatm/SimpleAtmApplication.java
@@ -2,8 +2,6 @@ package com.springboot.simpleatm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class SimpleAtmApplication {
